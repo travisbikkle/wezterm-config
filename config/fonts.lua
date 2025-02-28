4684ebc74800c3d24c6d82274feafaa39df365f5
travisbikkle <travisbikkle@proton.me>
2025-02-28 08:42:38 +0800

    change to a small sized font

--- config/fonts.lua
+++ config/fonts.lua
@@ -2,7 +2,8 @@ local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
 -- local font = 'Maple Mono SC NF'
-local font_family = 'JetBrainsMono Nerd Font'
+-- local font_family = 'JetBrainsMono Nerd Font'
+local font_family = 'Hack Nerd Font Mono'
 local font_size = platform.is_mac and 12 or 9
 
 return {
