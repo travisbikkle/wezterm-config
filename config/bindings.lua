e9fd2846596b922626ad84f22c504d4c7f049e6c
travisbikkle <travisbikkle@proton.me>
2025-02-28 08:44:09 +0800

    change leader key to ,

--- config/bindings.lua
+++ config/bindings.lua
@@ -243,7 +243,7 @@ local mouse_bindings = {
 return {
    disable_default_key_bindings = true,
    -- disable_default_mouse_bindings = true,
-   leader = { key = 'Space', mods = mod.SUPER_REV },
+   leader = { key = ',', mods = mod.SUPER_REV },
    keys = keys,
    key_tables = key_tables,
    mouse_bindings = mouse_bindings,
